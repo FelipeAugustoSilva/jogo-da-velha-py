51fd4e9a24198d8f36f6323d2c46a29989821c9c
FelipeAugusto <94065571+FelipeAugustoSilva@users.noreply.github.com>
2024-08-08 03:32:32 -0300

    Desenvolvendo

--- jogo-da-velha.py
+++ jogo-da-velha.py
@@ -18,8 +18,27 @@ class ticTacToe:
         self.done = ""
     
     def check_win_or_draw(self):
-        pass
-    
+        dict_win = {}
+        
+        for i in ["X", "O"]:
+            dict_win[i] = (self.board[0][0] == self.board[0][1] == self.board[0][2] == i)
+            dict_win[i] = (self.board[1][0] == self.board[1][1] == self.board[1][2] == i) or dict_win[i]
+            dict_win[i] = (self.board[2][0] == self.board[2][1] == self.board[2][2] == i) or dict_win[i]
+            
+            dict_win[i] = (self.board[0][0] == self.board[1][0] == self.board[2][0] == i) or dict_win[i]
+            dict_win[i] = (self.board[0][1] == self.board[1][1] == self.board[2][1] == i) or dict_win[i]
+            dict_win[i] = (self.board[0][2] == self.board[1][2] == self.board[2][2] == i) or dict_win[i]
+            
+            dict_win[i] = (self.board[0][0] == self.board[1][1] == self.board[2][2] == i) or dict_win[i]
+            dict_win[i] = (self.board[2][0] == self.board[1][1] == self.board[0][2] == i) or dict_win[i]
+            
+        if dict_win["X"]:
+            self.done = "x"
+            print("X venceu!")
+        elif dict_win["O"]:
+            self.done = "o"
+            print("O venceu!")   
+            
     def get_player_move(self):
         pass
     
